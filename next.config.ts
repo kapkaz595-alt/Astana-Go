efc9cf6477480fa433f1a5d1eabf6795ff602c3a
kapkaz595-alt <kapkaz595@gmail.com>
2026-09-02 18:18:04 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'example.com' },
       { protocol: 'https', hostname: '*.supabase.co' },
+      { protocol: 'https', hostname: 'pub-4365aa40f196442090288742fce80ce0.r2.dev' },
     ],
   },
 };
